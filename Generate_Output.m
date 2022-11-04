366fb7709f9375a4098ecd7499f7bebb064018d0
EdMHill <EdMHill@users.noreply.github.com>
2022-11-04 20:11:39 +0000

    Comments added to Generate_Output.m & ODE_to_Observables.m

--- Generate_Output.m
+++ Generate_Output.m
@@ -12,15 +12,14 @@ function Generate_Output(LOOP)
 
 % Outputs: None
 
-%%
+%% Set up variables
 
 % Flag variable. If true, outputs saved to MAT file.
 Matlab_Save=1;
 
-
 % Region associated values
-Rmax=11; % Maximum number of regions
-Rtot=[2:11]; %Region ID. 2:8 for the seven NHS regions of England. 9:11 for devolved nations.
+Rmax = 11;
+Rtot=2:11; %Region ID. 2:8 for the seven NHS regions of England. 9:11 for devolved nations.
 
 % Time horizon (in days)
 RUN_UNTIL=700;
@@ -36,12 +35,22 @@ Date=datenum(DATE_STR)+1-datenum(2020,1,1);
 load Surrogate_Vaccine_Data.mat % Vaccination uptake data
 load Regional_PP.mat % Population data
 
-% Main loop.
+%% Vaccine order set up.
 % If not running default vaccination scenario (LOOP = 0), amend vaccination
 % uptake variables.
 if LOOP>0 && LOOP<=3
+
+    % Storage arrays for daily amount of vaccine doses assigned to first doses
+    % and second doses (V1, V2 original surrogate data; VV1, VV2 to be populated with modified vaccine schedule data) 
     VV1=0*V1; VV2=0*V2;
-    for R=2:11
+        % 3D arrays:
+        % Row per region
+        % Column per day
+        % Slice per age group
+
+    for R=2:11 % Loop over each region
+        
+        % Set up list for vaccination order
         LIST=zeros(ceil(sum(V1(R,:,:),'all')),1);
         k=0; N=zeros(21,1);
         for t=1:size(V1,2)
@@ -61,11 +70,19 @@ if LOOP>0 && LOOP<=3
         else
             DLAY=3*7;  % 3 week delay
         end
+
+        % Get cumulative number of vaccine doses administered
         CumVacc=squeeze(cumsum(squeeze(sum(V1(R,:,:)+V2(R,:,:),3))));
         
+        % Populate VV1 & VV2
         k=0;
         for t=(DLAY+1):size(V1,2)
+            
+            % Assign second dose, DLAY days after first dose
             VV2(R,t,:)=VV1(R,t-DLAY,:);
+
+            % If any vaccine doses remain to be administered that day, assign
+            % rest as first doses
             Spare=round(CumVacc(t) - sum( VV1(R,1:t,:) + VV2(R,1:t,:) , 'all'));
             if Spare>0
                 VV1(R,t,:)=full(sparse(LIST(k+[1:Spare]),1,1,21,1));
@@ -73,8 +90,11 @@ if LOOP>0 && LOOP<=3
             end
         end
     end
+
+    % Assign revsied vaccination schedule to V1 & V2
     V1=VV1; V2=VV2;
 end
+
 
 if LOOP==2 % lower the efficacy against alpha and delta.
     %alpha
@@ -86,33 +106,47 @@ if LOOP==2 % lower the efficacy against alpha and delta.
 end
 
 
-% Random order of vaccination selected
+% Random order of vaccination scenario
 if LOOP==10
+
+    % Generate the random age-group order
     AGE_ORDER=[21:-1:4];
     AGE_ORDER=AGE_ORDER(randperm(length(AGE_ORDER)));
+
+    % Calculate number vaccinated per age group (& per region)
     Num_Vacc=round(squeeze(sum(V2,2))); Num_Vacc(:,1:3)=0; Num_Vacc(:,4)=round(Num_Vacc(:,4)*2/5);
-    Total_Daily_Vacc=round(squeeze(sum(V1+V2+V3,3)));
-    V1=0*V1; V2=0*V2;
     
+    % Calculate total number of vaccine doses administered per region
+    Total_Daily_Vacc=round(squeeze(sum(V1+V2+V3,3)));
+    
+    % Repopulate vaccine uptake arrays
+    V1=0*V1; V2=0*V2;
     for Region=2:11
         RNV=Num_Vacc(Region,:);
         for T=(Dlay+1):length(Total_Daily_Vacc)
             Tot=Total_Daily_Vacc(Region,T);
             Scnds=squeeze(sum(V1(Region,1:(T-Dlay),:),2)-sum(V2(Region,1:T,:),2));
-            if Tot<sum(Scnds)  % not enough vaccine today
+            if Tot<sum(Scnds)  % not enough vaccine today to assign all second doses
+                % Assign doses to each age group proportionally to number
+                % of required doses in that age group
                 tmp=zeros(sum(Scnds),1); k=0; for i=1:21, tmp(k+[1:Scnds(i)])=i; k=k+Scnds(i); end;
                 ptmp=tmp(randperm(length(tmp)));
                 ptmp((Tot+1):end)=[];
                 V2(Region,T,:)=hist(ptmp,[1:21]);
             else
+                % Enough vaccines to administer all scheduled second doses
                 V2(Region,T,:)=Scnds;
                 Tot=Tot-sum(Scnds);
+
+                % With remaining doses, assign as first doses in specified
+                % AGE_ORDER
                 o=1;
                 while o<=length(AGE_ORDER) & Tot>0
                     A=AGE_ORDER(o);
                     if Tot>RNV(A)
                         V1(Region,T,A)=RNV(A); Tot=Tot-RNV(A); RNV(A)=0;
                     else
+                        % All doses on current day now used
                         V1(Region,T,A)=Tot; RNV(A)=RNV(A)-Tot; Tot=0;
                     end
                     o=o+1;
@@ -127,10 +161,7 @@ clear nALL nDEATHS nHOSP_AD nHOSP_OCC nICU_OCC nICU_AD
 
 Names={'England','East of England','London','Midlands','North East and Yorkshire','North West','South East','South West','Wales','Scotland','Northern Ireland','United Kingdom'};
 
-%%
-% generate run_stops by region
-Comps = zeros(4,Num_Comp+1,11);
-
+%% Generate run_stops by region
 RUN_STOP(RUN_STOP>RUN_UNTIL)=[];
 Num_Comp=length(RUN_STOP);
 
@@ -138,8 +169,6 @@ RUN_STOPs = zeros(11,Num_Comp);
 for Region = 2:11
     RUN_STOPs(Region,1:length(RUN_STOP)) = RUN_STOP;
 end
-
-Keep_RUN_STOPs=RUN_STOPs(2,:);
 
 if size(V1,2)<max(RUN_STOPs,[],'all')
     mx=max(RUN_STOPs,[],'all')+100;
@@ -150,8 +179,9 @@ if size(V1,2)<max(RUN_STOPs,[],'all')
     end
 end
 
-%%
+%% Run the model
 
+% Initialise output arrays
 maxtime = max(RUN_STOPs(end,:))+30; % needs to be big enough to take account of any lags
 nALL = zeros(11,maxtime,105);
 nDEATHS = zeros(11,maxtime,105);
@@ -163,26 +193,24 @@ nICU_OCC = zeros(11,maxtime,105);
 % generate Comp by region
 clear Comps
 
-RUN_STARTs=[82*ones(11,1) RUN_STOPs(:,1:(end-1))];
-
 % ADD IN DECLINE IN PRECAUTIONARY BEHAVIOUR IF SIMULATION LENGTH
 % EXCEEDS DATA.
 NC=Num_Comp;
+Comps = zeros(Rmax,size(RUN_STOPs,2));
+CompsO = zeros(Rmax,size(RUN_STOPs,2));
 for Region = Rtot
     Comps(Region,1:NC)=nCOMPLIANCE(1:NC,Region);
     Comps(Region,NC:size(RUN_STOPs,2))=Comps(Region,NC);
     CompsO(Region,1:NC)=nCOMPLIANCEO(1:NC,Region);
     CompsO(Region,NC:size(RUN_STOPs,2))=CompsO(Region,NC);
     m=find(RUN_STOPs(Region,:)>Date);  Decline=0.025;
-    if length(m)
+    if ~isempty(m)
         for i=1:length(m)
             Comps(Region,m(i))=max(Comps(Region,m(i)-1)-Decline,0.001);  %Drop compliance slowly each week until zero
         end
         CompsO(Region,m)=CompsO(Region,m(1)-1)*Comps(Region,m)/Comps(Region,m(1)-1);
     end
 end
-
-MAX_REGION=Rmax;
 
 TXT_STR=['Warwick_Output_Loop' num2str(LOOP)];
 
@@ -191,37 +219,41 @@ for Region=2:8
     
     fprintf(1,'Simulating %s, Vaccine Loop %d\n',Names{Region},LOOP);
     
+    % Run the model
     [nDC, ~, ~, nHospital, inHospital, nICU, inICU, nDeaths, ~, All, ~]=Simulate_One_Region(Region, nTAU, nALPHA, nINC_P, nSCALING(Region), nFACTOR(Region), nH_FACTOR(Region:11:end), nI_FACTOR(Region:11:end), nD_FACTOR(Region:11:end),  ...
         nH_STRETCH(Region:11:end), nI_STRETCH(Region:11:end), nLAG(Region:11:end), START_DATE(Region)+1, 1, Comps(Region,:), CompsO(Region,:), RUN_STOPs(Region,:), nNV_BETA(Region:11:end)', nNV_SPEED(Region:11:end)', nIMPORT_DAY(Region:11:end), nIMPORT_LEVEL(Region:11:end),...
         squeeze(V1(Region,:,:)), squeeze(V2(Region,:,:)), squeeze(V3(Region,:,:)), Transmission_Reduction, VEffI, VEffS, VEffH, VEffD, RatioPf,...
         Detection, Susceptibility, nGAMMA, WaningSpeed, WaningEfficacy, [0.1 0], MaxType, [], 0);
-
+    
+    % Modify deaths, based on hospital occupancy
     nDeaths=nDeaths.* ((1+nDH_SCALING(Region)*sum(inHospital,2)./sum(Region_PP(Region,:),2))*ones(1,size(nDeaths,2)));
     
+    % Assign model outcomes to output storage arrays
     T=1:size(All,1);
     
     padding = maxtime-length(T);
-    nALL(Region,:,:)=[All; zeros(padding,105)];
-    nDEATHS(Region,:,:)=[nDeaths; zeros(padding,105)];
-    nHOSP_OCC(Region,:,:)=[inHospital; zeros(padding,105)];
-    nHOSP_AD(Region,:,:) = [nHospital; zeros(padding,105)];
-    nICU_OCC(Region,:,:) = [inICU; zeros(padding,105)];
-    nICU_AD(Region,:,:) = [nICU; zeros(padding,105)];
+    nALL(Region,:,:)=[All; zeros(padding,105)]; % infections
+    nDEATHS(Region,:,:)=[nDeaths; zeros(padding,105)]; % deaths
+    nHOSP_OCC(Region,:,:)=[inHospital; zeros(padding,105)]; % hospital occupancy
+    nHOSP_AD(Region,:,:) = [nHospital; zeros(padding,105)]; % hospital admissions
+    nICU_OCC(Region,:,:) = [inICU; zeros(padding,105)]; % ICU occupancy
+    nICU_AD(Region,:,:) = [nICU; zeros(padding,105)]; % ICU admissions
 end
 
 
 
-%% NEED TO COMBINE THE OLD & NEW VARIANT !
+%% SUM EPIDEMIOLOGICAL OUTCOMES ACROSS THE VARIANTS
 
 A=1:size(nALL,3); a=1:21; b=21+a; c=42+a; d=63+a; e=84+a;
-nALL=nALL(:,:,a)+nALL(:,:,b)+nALL(:,:,c)+nALL(:,:,d)+nALL(:,:,e);
-nDEATHS=nDEATHS(:,:,a)+nDEATHS(:,:,b)+nDEATHS(:,:,c)+nDEATHS(:,:,d)+nDEATHS(:,:,e);
-nHOSP_OCC=nHOSP_OCC(:,:,a)+nHOSP_OCC(:,:,b)+nHOSP_OCC(:,:,c)+nHOSP_OCC(:,:,d)+nHOSP_OCC(:,:,e);
-nHOSP_AD=nHOSP_AD(:,:,a)+nHOSP_AD(:,:,b)+nHOSP_AD(:,:,c)+nHOSP_AD(:,:,d)+nHOSP_AD(:,:,e);
-nICU_OCC=nICU_OCC(:,:,a)+nICU_OCC(:,:,b)+nICU_OCC(:,:,c)+nICU_OCC(:,:,d)+nICU_OCC(:,:,e);
-nICU_AD=nICU_AD(:,:,a)+nICU_AD(:,:,b)+nICU_AD(:,:,c)+nICU_AD(:,:,d)+nICU_AD(:,:,e);
+nALL=nALL(:,:,a)+nALL(:,:,b)+nALL(:,:,c)+nALL(:,:,d)+nALL(:,:,e); % infections
+nDEATHS=nDEATHS(:,:,a)+nDEATHS(:,:,b)+nDEATHS(:,:,c)+nDEATHS(:,:,d)+nDEATHS(:,:,e); % deaths
+nHOSP_OCC=nHOSP_OCC(:,:,a)+nHOSP_OCC(:,:,b)+nHOSP_OCC(:,:,c)+nHOSP_OCC(:,:,d)+nHOSP_OCC(:,:,e); % hospital occupancy
+nHOSP_AD=nHOSP_AD(:,:,a)+nHOSP_AD(:,:,b)+nHOSP_AD(:,:,c)+nHOSP_AD(:,:,d)+nHOSP_AD(:,:,e); % hospital admissions
+nICU_OCC=nICU_OCC(:,:,a)+nICU_OCC(:,:,b)+nICU_OCC(:,:,c)+nICU_OCC(:,:,d)+nICU_OCC(:,:,e); % ICU occupancy
+nICU_AD=nICU_AD(:,:,a)+nICU_AD(:,:,b)+nICU_AD(:,:,c)+nICU_AD(:,:,d)+nICU_AD(:,:,e); % ICU admissions
 
 
+% If flag variable active, save outcomes to file
 if Matlab_Save
     save([TXT_STR '_' DATE_STR '.mat'],'nALL','nDEATHS','nHOSP_OCC','nHOSP_AD','nICU_OCC','nICU_AD');
 end
--- ODE_to_Observables.m
+++ ODE_to_Observables.m
@@ -2,14 +2,41 @@ function [NDC, nHospital, inHospital, nICU, inICU, nDeaths, INF]=ODE_to_Observab
     Assumed_Delay_Reporting_Deaths, rc_Distribution_Hosp_Time, rc_Distribution_HospICU_Time, rc_Distribution_ICU_Time, Distribution_Symptoms_to_Hospital, Distribution_Symptoms_to_ICU, Distribution_Hopital_to_Death, ...
     Hosp_2_Death, Sympt_2_critcal, Sympt_2_hosp, Wales_Flag)
 
+% Inputs:
+% T - Output evaluation times 
+% nDC - New symptomatic cases array
+% E - Latent infected counts
+% Region - Region ID
+% h_factor,i_factor - Modification factors for symptomatic case to hospitalisation/ICU admission probabilities
+% d_factor - Modification factor for hospitalisation to death probabilities
+% h_stretch,i_stretch - Modification factors for hospital & ICU length of stay distributions  
+% a - Rate out of latent/exposed disease state
+% Assumed_Delay_Reporting_Deaths - Region specific death reporting delay
+% rc_Distribution_Hosp_Time, rc_Distribution_ICU_Time - Length of stay distributions for hospital admission & ICU admission
+% rc_Distribution_HospICU_Time - Distribution of hospital to ICU time.
+% Distribution_Symptoms_to_Hospital, Distribution_Symptoms_to_ICU, Distribution_Hopital_to_Death - Distirubtions for time to progress between specified severe health episode stages
+% Hosp_2_Death, Sympt_2_critcal, Sympt_2_hosp - Age-group specific probabilities for specified severe health episode progression
+% Wales_Flag - If active, runs observation loop with a 14 day cut-off to match data feed
 
+% Outputs: 
+% 2D arrays. Row per timestep, column per age group
+%   NDC - Symptomatic cases
+%   nHospital - Hospital admissions
+%   inHospital - Hospital occupancy
+%   nICU - ICU admissions
+%   inICU - ICU occpancy
+%   nDeaths - Deaths
+%   INF - Infections
+
+% Assign number of age groups
 nA=size(nDC,2);
 
+% Scaling factor for hospital length of stay stretch parameter (per age group)
 Age_Factor=[2.595 2.595 2.595 2.595 2.595 2.595 1.9752 1.9752 1.2771 ...
     1.2771 1.1319 1.1319 0.9547 0.9547 0.7959 0.7959 0.8854 0.8854 ...
     0.8854 0.8854 0.8854];
 
-%new Hospital
+%new hospital admissions
 nHospital=zeros(max(T)+100,nA);
 A=1:nA;
 LL=[1:length(Distribution_Symptoms_to_Hospital)];
@@ -17,7 +44,7 @@ for t=1:length(T)
     nHospital(T(t)+LL,A)=nHospital(T(t)+LL,A) + Distribution_Symptoms_to_Hospital'*(nDC(T(t),A).*Prob_Modifier_linear(Sympt_2_hosp(A),h_factor)');
 end
 
-%new ICU
+%new ICU admissions
 nICU=zeros(max(T)+100,nA);
 LL=[1:length(Distribution_Symptoms_to_ICU)];
 for t=1:length(T)
@@ -47,8 +74,8 @@ end
 nHospital((max(T)+1):end,:)=[];
 inHospital((max(T)+1):end,:)=[];
 
-A=1:nA;
 %in ICU
+A=1:nA;
 inICU=zeros(max(T)+ceil(100*i_stretch),nA);
 l=length(rc_Distribution_ICU_Time)-1;
 tmpDist_ICU_Time=interp1([0:l],rc_Distribution_ICU_Time,[0:(1/i_stretch):l],'linear')';
@@ -59,7 +86,8 @@ end
 nICU((max(T)+1):end,:)=[];
 inICU((max(T)+1):end,:)=[];
 
-if Region==9 & Wales_Flag % if Wales_Flag set !!  DO IT ALL AGAINST BUT WITH 14 day cut-off to match data feeds
+% Wales: If Wales_Flag set to 1 (true), run again with a 14 day cut-off to match data feed
+if Region==9 & Wales_Flag 
     inHospital2=zeros(max(T)+ceil(100*h_stretch),nA);
     l=length(rc_Distribution_Hosp_Time)-1;
     tmpDist_Hosp_Time=interp1([0:l],rc_Distribution_Hosp_Time,[0:(1/h_stretch):l],'linear')';
@@ -92,21 +120,26 @@ if Region==9 & Wales_Flag % if Wales_Flag set !!  DO IT ALL AGAINST BUT WITH 14 
     inICU(1:MM,:)=inICU2(1:MM,:);
 end
 
-% find Deaths
+% Deaths
 nDeaths=zeros(max(T)+150,nA);
 LL=[1:length(Distribution_Hopital_to_Death)];
 for t=1:length(T)
     nDeaths(T(t)+LL+Assumed_Delay_Reporting_Deaths(Region),A)=nDeaths(T(t)+LL+Assumed_Delay_Reporting_Deaths(Region),A) + Distribution_Hopital_to_Death'*(nHospital(T(t),A).*Prob_Modifier_linear(Hosp_2_Death(A),d_factor)');
 end
-NDC=nDC(1:T(end),:);
 nDeaths((max(T)+1):end,:)=[];
 
+% Infections
 INF=zeros(T(end),21);
 if ~isempty(E)
     INF(T,:)=E*a;
 end
+
+% Symptomatic cases
+NDC=nDC(1:T(end),:);
+
 end
 
+%% Supporting functions
 function [Y] = Prob_Modifier_linear(y,x)
 
 Y=y.*x;
